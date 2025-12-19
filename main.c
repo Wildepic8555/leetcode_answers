a85dfe6c8a988e5e96df81abae369de157d5a2e3
Wildepic8555 <wildepic255555@outlook.com>
2025-12-19 13:03:48 +0100

    updated

--- main.c
+++ main.c
@@ -339,3 +339,62 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
     qsort(nums1, m + n, sizeof(int), comp);
 }
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Constraints:
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+*/
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+    int prefixLen = 0;
+    int i = 0;
+    bool isTrue = true;
+    int sameCount = 0;
+
+    if(strcmp(strs[0], "") == 0){
+        isTrue = false;
+    }
+    else if(strsSize == 1){
+        strs[0][1] = '\0';
+        return strs[0];
+    }
+
+    for(i = 0; i < strsSize ; i++){
+        if(strcmp(strs[i], strs[0]) == 0){
+            sameCount++;
+        }
+    }
+    if(sameCount == strsSize){
+        return strs[0];
+    }
+
+    while(isTrue){
+        for(i = 0; i < strsSize && strsSize > 1; i++){
+            if(strs[i][prefixLen] != strs[0][prefixLen]){
+                isTrue = false;
+            }
+        }
+        if(isTrue ){
+            prefixLen++;
+        }
+        else {
+            isTrue = false;
+        }
+    }
+    strs[0][prefixLen] = '\0';
+    return strs[0];
+}
