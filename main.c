3f13083ea9f7fdca988a436becc50a2bf2bba3ca
Wildepic <138814330+Wildepic8555@users.noreply.github.com>
2026-02-09 15:12:33 +0100

    Add function to find single number in array
    Implement function to find the single number in an array where every element appears twice except for one.

--- main.c
+++ main.c
@@ -439,3 +439,71 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize = 2; // Critical: Tell LeetCode the array size
     return result;
 }
+
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+*/
+
+/*
+int comp(const void *a, const void *b) {
+    int A = *(const int *)a;
+    int B = *(const int *)b;
+
+    if (A < B) return -1;
+    if (A > B) return 1;
+    return 0;
+}
+*/
+
+
+int deleteNumbers( int array[], int noe, int trigger ) {
+    int deleted = 0;
+
+    for ( int i=0; i < noe; i++ ) {
+        if ( array[i] == trigger ) {
+            deleted++;
+            noe = noe -1;
+
+            for (int ii = i ; ii < noe; ii++) {
+                array[ii] = array[ii+1];
+            }
+            i--;
+        }
+    }
+    return deleted;
+}
+
+int singleNumber(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), comp);
+    int i = 0;
+
+
+    while (1 < numsSize) {
+        if(nums[0] == nums[1]){
+            numsSize -= deleteNumbers(nums , numsSize , nums[0]);
+        }
+        else{
+            return nums[0];
+        }
+    }
+
+    return nums[0];
+}
